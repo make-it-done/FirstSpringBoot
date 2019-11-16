b6bbd30d8a96933c4362159449670d209ff7b1b5
Rohit Kumar <rohitislampur@gmail.com>
2019-11-17 02:00:07 +0530

    adding all extra spring boot rest change

--- src/main/java/com/rohit/FirstSpringBoot/DAO/CustomerRepository.java
+++ src/main/java/com/rohit/FirstSpringBoot/DAO/CustomerRepository.java
@@ -1,9 +1,11 @@
 package com.rohit.FirstSpringBoot.DAO;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.rohit.FirstSpringBoot.Entity.Customer;
 
+@RepositoryRestResource(path="members")
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 
 }
